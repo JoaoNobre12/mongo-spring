597ded834e2cd2ef2483c31801be0d225c4e00f2
joao nobre <joaovitornobre12@hotmail.com>
2024-09-14 23:26:51 -0300

    adds hello world

--- mongo-spring/src/main/java/com/casa/mongospring/controller/UserController.java
+++ mongo-spring/src/main/java/com/casa/mongospring/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.casa.mongospring.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @RequestMapping("/user")
+    public String user() {
+        return "User";
+    }
 }
